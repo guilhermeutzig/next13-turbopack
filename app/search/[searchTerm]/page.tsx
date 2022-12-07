d3fe6f158ddb8fe8fc0777cb7994411df1c27c59
Guilherme Utzig <guilhermelutzig@gmail.com>
2022-12-07 10:33:06 -0300

    Added Serp API call; Added .env.example

--- app/search/[searchTerm]/page.tsx
+++ app/search/[searchTerm]/page.tsx
@@ -1,3 +1,21 @@
-export default function SearchTermPage() {
+type Props = {
+  params: {
+    searchTerm: string;
+  };
+};
+
+const search = async (searchTerm: string) => {
+  const res = await fetch(
+    `https://serpapi.com/search.json?q=${searchTerm}&api_key=${process.env.API_KEY}`,
+  );
+  const data = await res.json();
+  return data;
+};
+
+async function SearchTermPage({ params: { searchTerm } }: Props) {
+  const searchResults = await search(searchTerm);
+
   return <div>search results</div>;
 }
+
+export default SearchTermPage;
